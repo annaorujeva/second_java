297551ae08f7d3eec108cc17fe5c68a08023f89a
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-09-25 14:09:36 +0300

    Create Circle.java

---
+++ Shape/Circle.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Circle implements Shape{
+    public double r;
+    public Circle(double radius){
+        r = radius;
+    }
+
+    public double square(){
+        return Math.PI*(this.r * this.r);
+    }
+}
