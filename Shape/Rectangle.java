e4409b370aac9b1166fcb59c1e9d408ad8345c76
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-09-25 14:08:40 +0300

    Create Rectangle.java

---
+++ Shape/Rectangle.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Rectangle implements Shape{
+    public double a;
+    public double b;
+    public Rectangle(double A, double B){
+         a = A;
+         b= B;
+    }
+
+    public double square(){
+        return this.a * this.b;
+    }
+
+}
