faa1370af14ff994ba6c1e2fe59abfd2622e799b
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-09-25 15:23:38 +0300

    Update Engine.java

--- Car/Engine.java
+++ Car/Engine.java
@@ -3,4 +3,8 @@ package app;
 public class Engine {
     public double power;
     public String producer;
+    public Engine(String producer, double power){
+        this.power = power;
+        this.producer = producer;
+    }
 }
