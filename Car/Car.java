792131ad05d23cc9ba7466dfefd11e9fd8e5e57e
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-09-25 15:46:47 +0300

    Update Car.java

--- Car/Car.java
+++ Car/Car.java
@@ -4,7 +4,7 @@ package app;
 Класс Car содержит поля - марка автомобиля, класс автомобиля, вес, мотор типа Engine.
 Абстрактные методы start(), stop(), и реализованные методы turnRight(), turnLeft()
 Методы turnRight() и turnLeft() выводят на экран: "Поворот направо" или "Поворот налево".
-Абстрактный метод printInfo(), который выводит полную информацию об автомобиле, и моторе. (??? - как это сделать, разве абстрактный метод может что-то делать?)
+Абстрактный метод printInfo(), который выводит полную информацию об автомобиле, и моторе. 
 */
 
 
@@ -14,19 +14,21 @@ public abstract class Car {
     public double weight;
     public Engine motor;
 
-    public Car(String abrand, String aclassofcar, double aweight){
+    public Car(String abrand, String aclassofcar, double aweight, Engine amotor){
         this.brand = abrand;
         this.classofcar = aclassofcar;
         this.weight = aweight;
+        this.motor = amotor;
     }
 
     public abstract void stop();
     public abstract void start();
+    public abstract String printInfo();
 
     public void turnRight(){
         System.out.println("Поворот направо");
     }
-    public void turnLeft{
+    public void turnLeft(){
         System.out.println("Поворот налево");
     }
 
