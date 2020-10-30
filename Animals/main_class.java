fb95a3d3dca6472b1ffda7104a9f9c04ac7e8c7f
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-10-30 13:57:41 +0300

    Update main_class.java

--- Animals/main_class.java
+++ Animals/main_class.java
@@ -1,17 +1,25 @@
+//Здесь тестируются написанные классы
 package app;
 
 public class main_class {
     public static void main(String[] args){
         Animal[] animals = new Animal[3];
         Doctor AnnaDoctor = new Doctor();
-        animals[0] = new Dog("Bones", "House #1", "Dog");
-        animals[1] = new Cat("Fish", "House #2", "Cat");
-        animals[2] = new Horse("Grass", "House #3", "Horse");
+        animals[0] = new Dog("Oddi","Bones", "House #1", "Dog");
+        animals[1] = new Cat("Jane","Fish", "House #2", "Cat");
+        animals[2] = new Horse("Qwer","Grass", "House #3", "Horse");
         for (Animal animal : animals) {
             AnnaDoctor.treatAnimal(animal);
         }
-        Dog doggy = new Dog("aa","bb","cc");
-        doggy.setWeight(9);
+        Dog doggy = new Dog("Никки","корм","квартира","маленький пес");
+        doggy.setWeight(5);
         System.out.println(doggy.getWeight());
+        System.out.println(doggy.getTypeofanimal());
+        doggy.sleep();
+        doggy.eat();
+        doggy.makeNoise();
+        animals[2].eat();
+        animals[2].makeNoise();
+        animals[2].sleep();
     }
 }
