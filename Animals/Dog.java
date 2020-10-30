f3e47e82232a44ed6b98a728e1f2f2caac889a68
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-10-30 13:56:43 +0300

    Update Dog.java

--- Animals/Dog.java
+++ Animals/Dog.java
@@ -3,13 +3,28 @@ package app;
 public class Dog extends Animal{
     private double weight;
 
-    public Dog(String aFood, String aLocation, String aType) {
-        super(aFood, aLocation, aType);
+    public Dog(String aName, String aFood, String aLocation, String aType) {
+        super(aName, aFood, aLocation, aType);
     }
     public void setWeight(float aWeight){
         this.weight = aWeight;
     }
     public double getWeight(){
         return weight;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Собака " + name +" гавкает и делает кусь");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Собака "+ name +" ест тапки и " + food);
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Собака " + name +" спит");
     }
 }
