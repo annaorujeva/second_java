7fb3b55680943ff91b5a2bcde4f48fd6756bc6c8
annaorujeva <71508284+annaorujeva@users.noreply.github.com>
2020-10-30 13:56:23 +0300

    Update Cat.java

--- Animals/Cat.java
+++ Animals/Cat.java
@@ -3,13 +3,28 @@ package app;
 public class Cat extends Animal{
     private int numofkittens;
 
-    public Cat(String aFood, String aLocation, String aType) {
-        super(aFood, aLocation, aType);
+    public Cat(String aName, String aFood, String aLocation, String aType) {
+        super(aName, aFood, aLocation, aType);
     }
-    public void setNumofkittens(int count){
-        this.numofkittens = count;
+    public void setNumofkittens(int countKittens){
+        this.numofkittens = countKittens;
     }
     public int getNumofkittens(){
         return numofkittens;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Кот " + name +" мяукает");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Кот " + name + " ест " + food);
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Кот " + name +" спит");
     }
 }
